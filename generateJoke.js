9916aa65aa02d0e528a9b6ce9c54bc8d2dd6ec24
vuthy <vuthy104.infinity@gmail.com>
2023-05-05 11:31:33 +0700

    Initial commit

---
+++ generateJoke.js
@@ -0,0 +1,6 @@
+function generateJoke() {
+    return 'generate Joke';
+}
+console.log(1)
+
+export default generateJoke;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import { v4 as uuidv4 } from 'uuid'
+import './styles/main.scss'
+
+import generateJoke from "../generateJoke";
+
+console.log(uuidv4())
+
